9ff6df10712d523208ec2ecea7eb888697331e75
Francortiz-137 <francortiz.c@gmail.com>
2022-11-30 11:52:25 -0300

    Fix service endpoints tested on postman

--- src/main/java/com/employeemanager/employeemanager/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/employeemanager/employeemanager/service/impl/EmployeeServiceImpl.java
@@ -5,11 +5,13 @@ import com.employeemanager.employeemanager.model.Employee;
 import com.employeemanager.employeemanager.repo.EmployeeRepo;
 import com.employeemanager.employeemanager.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
 
     private final EmployeeRepo employeeRepo;
