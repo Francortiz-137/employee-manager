d59afc0e326033b5475acdcc47b3e9f99355e7a5
Francortiz-137 <francortiz.c@gmail.com>
2022-11-30 10:55:34 -0300

    Created EmployeeController and get methods

---
+++ src/main/java/com/employeemanager/employeemanager/controller/EmployeeController.java
@@ -0,0 +1,34 @@
+package com.employeemanager.employeemanager.controller;
+
+import com.employeemanager.employeemanager.model.Employee;
+import com.employeemanager.employeemanager.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees (){
+        List<Employee> employeeList = employeeService.findAllEmployees();
+        return new ResponseEntity<>(employeeList, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Employee> getEmployeeById (@PathVariable("id") Long id){
+        Employee employee = employeeService.findEmployeeById(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+}
--- src/main/java/com/employeemanager/employeemanager/service/EmployeeService.java
+++ src/main/java/com/employeemanager/employeemanager/service/EmployeeService.java
@@ -1,7 +1,19 @@
 package com.employeemanager.employeemanager.service;
 
+import com.employeemanager.employeemanager.model.Employee;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface EmployeeService {
+    public Employee addEmployee(Employee employee);
+
+    public List<Employee> findAllEmployees();
+
+    public Employee updateEmployee(Employee employee);
+
+    public Employee findEmployeeById(Long id);
+
+    public void deleteEmployee(Long id);
 }
